88252d452bb7299baed68dee7c38ba7b74973a08
Glad Khumalo <pynathonc@gmail.com>
2026-05-25 21:22:18 +0200

    Create a folder private_key_perm

--- System-Health-Maintenance/disk-space-monitor.ps1
+++ System-Health-Maintenance/disk-space-monitor.ps1
@@ -1,10 +1,6 @@
 <#
 .SYNOPSIS
-<<<<<<< HEAD
     Monitors disk space on local or remote servers and sends alerts if below threshold
-=======
-    Monitors disk space on local or remote servers and sends alerts if below threshold.
->>>>>>> f8fd753002310f44a37ca0e8fd6eb9586cbab12d
 
 .PARAMETER ComputerName
     One or more computer names (default: local machine)
